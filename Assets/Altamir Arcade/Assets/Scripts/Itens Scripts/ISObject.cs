06fe8561577d7eae99b7ebb5f4f01c3b5a2d2fa5
Altamir Benkenstein <altamir.benkenstein@outlook.com>
2016-02-03 17:41:58 -0200

    aula01:
    Criada Interfaces

---
+++ Assets/Altamir Arcade/Assets/Scripts/Itens Scripts/ISObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ISObject : IISObject
+{
+    [SerializeField] Sprite _icon;
+    [SerializeField] string _name;
+    [SerializeField] int _value;
+    [SerializeField] int _burden;
+    [SerializeField] ISQuality _quality;
+
+    public int ISBurden
+    {
+        get { return _burden; }
+        set { _burden = value; }
+    }
+
+    public Sprite ISIcon
+    {
+        get { return _icon; }
+        set { _icon = value; }
+    }
+
+    public string ISName
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public ISQuality ISQuality
+    {
+        get { return _quality; }
+        set { _quality = value; }
+    }
+
+    public int ISValue
+    {
+        get { return _value; }
+        set { _value = value; }
+    }
+}
---
+++ Assets/Altamir Arcade/Assets/Scripts/Itens Scripts/Interfaces/IISObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IISObject
+{
+    string ISName { get; set; }
+    int ISValue { get; set; }
+    Sprite ISIcon { get; set; }
+	int ISBurden { get; set; }
+    ISQuality ISQuality { get; set; }
+}
